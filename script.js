2949be43f394251a918f05921893b54864748ef8
Zhumabekuly Nurislam <nurislamzumabek49@gmail.com>
2024-10-31 16:27:23 +0600

    ??

---
+++ script.js
@@ -0,0 +1,40 @@
+// var x = 30;
+// var y = 40;
+
+// var z = x +y; //Арифметикалық оператця
+
+// // console.log(z);
+
+// let a = 20;
+// let b = 25; //Тағайындау /меншіктеу
+
+// //console.log(a-b); //Азайту аперетцясы
+
+// let total = a*b; //Kobity operiatsiasy
+// console.log(total)
+
+// let num1 = 50;
+// let num2 =30;
+
+// console.log(num1 % 2 =0 )
+
+// let num1 = 53; // tagaiynday
+// // = = ten ekenin tekseru
+// if (num1 % 2 = 0 )
+
+//     let num = 100;
+//     let num2 = 3;
+
+//     num %= num2;
+
+//     console.log(num); // 1
+
+// var a = 20;
+// var b = 30;
+// console.log(a+b)
+
+var a =5;
+var b =4;
+var c = 2;
+var z = 10
+console.log((5*4)/2/z-3)
